fc1c44fcbdc8db18d413ced0a7e39e5b0ed148d2
hikaruu <no.noo.nooo.yes@gmail.com>
2016-06-30 11:49:59 +0700

    AI-145.2949926 <Long@Long-PC Overwrite remote https://hikaruu@bitbucket.org/HikaTeam/vchat.git to local

--- V_CHAT/src/main/java/com/vinhsang/demo/v_chat/sampleio/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.vinhsang.demo.v_chat.sampleio;
-
-public class Constants {
-    //ko ket noi dc neu dung localhost:3000
-    //public static final String CHAT_SERVER_URL = "http://10.0.12.14:3000";
-    public static final String CHAT_SERVER_URL = "http://10.0.12.14:3003";
-}
