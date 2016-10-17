5d31e6ff2c93a68ab33b9754107211ae1c21c242
beyonderVN <no.noo.nooo.yes@gmail.com>
2016-10-17 13:07:27 +0700

    ahihi

---
+++ mobile/src/main/java/com/longngo/moviebox/di/MainComponent.java
@@ -0,0 +1,30 @@
+package com.longngo.moviebox.di;
+
+import android.content.Context;
+
+
+import com.longngo.moviebox.ui.activity.detail.DetailActivity;
+import com.longngo.moviebox.ui.activity.detail.DetailUsingOnlyRVActivity;
+import com.longngo.moviebox.ui.activity.main.MainActivity;
+import com.ngohoang.along.appcore.data.backend.MovieBoxService;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Long on 7/8/2016.
+ */
+@Singleton
+@Component(modules = {MainModule.class})
+public interface MainComponent {
+    Context context();
+    MovieBoxService movieBoxService();
+    void inject(MainActivity mainActivity);
+
+
+    void inject(DetailActivity detailActivity);
+    void inject(DetailUsingOnlyRVActivity detailUsingOnlyRVActivity);
+
+
+}
